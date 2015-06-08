89ce0b2d8f4098da83fc219db4a88bfe4c5916ab
twalder-docnet <tom@doc-net.com>
2015-06-08 23:44:05 +0100

    Add new result map test

--- tests/ProtoBufFetchTest.php
+++ tests/ProtoBufFetchTest.php
@@ -75,9 +75,11 @@ class ProtoBufFetchTest extends GDSTest {
     }
 
     /**
-     * Fetch by Id
+     * Create a basic "fetch by ID" request for re-use
+     *
+     * @return \google\appengine\datastore\v4\LookupRequest
      */
-    public function testFetchById()
+    private function getBasicBookByIdRequest()
     {
         $obj_request = new \google\appengine\datastore\v4\LookupRequest();
         $obj_request->mutableReadOptions();
@@ -87,11 +89,47 @@ class ProtoBufFetchTest extends GDSTest {
         $obj_kpe = $obj_key->addPathElement();
         $obj_kpe->setKind('Book');
         $obj_kpe->setId(123456789);
+        return $obj_request;
+    }
 
-        $this->apiProxyMock->expectCall('datastore_v4', 'Lookup', $obj_request, new \google\appengine\datastore\v4\LookupResponse());
-
+    /**
+     * Fetch by Id
+     */
+    public function testFetchById()
+    {
+        $this->apiProxyMock->expectCall('datastore_v4', 'Lookup', $this->getBasicBookByIdRequest(), new \google\appengine\datastore\v4\LookupResponse());
         $obj_result = $this->createBasicStore()->fetchById(123456789);
         $this->assertEquals($obj_result, NULL);
+        $this->apiProxyMock->verify();
+    }
+
+    /**
+     * Fetch by Id and process basic result
+     */
+    public function testFetchByIdWithResult()
+    {
+
+        $obj_response = new \google\appengine\datastore\v4\LookupResponse();
+        $obj_found = $obj_response->addFound();
+        $obj_entity = $obj_found->mutableEntity();
+        $obj_result_key = $obj_entity->mutableKey();
+        $obj_result_kpe = $obj_result_key->addPathElement();
+        $obj_result_kpe->setKind('Book');
+        $obj_result_kpe->setId(123456789);
+        $obj_result_property = $obj_entity->addProperty();
+        $obj_result_property->setName('author');
+        $obj_val = $obj_result_property->mutableValue(); // addDeprecatedValue();
+        $obj_val->setStringValue('William Shakespeare');
+
+        $this->apiProxyMock->expectCall('datastore_v4', 'Lookup', $this->getBasicBookByIdRequest(), $obj_response);
+
+        $obj_result = $this->createBasicStore()->fetchById(123456789);
+
+        $this->assertInstanceOf('\\GDS\\Entity', $obj_result);
+        $this->assertEquals(1, count($obj_result->getData()));
+        $this->assertEquals('Book', $obj_result->getKind());
+        $this->assertEquals(123456789, $obj_result->getKeyId());
+        $this->assertEquals($obj_result->author, 'William Shakespeare');
 
         $this->apiProxyMock->verify();
     }
