5667cd46af49ccf4c1d031b52d2f2d3c36c883b9
twalder-docnet <tom@doc-net.com>
2015-06-13 10:59:22 +0100

    Whitespace typo

--- tests/SchemaTest.php
+++ tests/SchemaTest.php
@@ -20,7 +20,7 @@
  *
  * @author Tom Walder <tom@docnet.nu>
  */
-class SchemaTest extends\PHPUnit_Framework_TestCase {
+class SchemaTest extends \PHPUnit_Framework_TestCase {
 
     /**
      * Set up a schema with all data types
