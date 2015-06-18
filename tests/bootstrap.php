041c0f877b9fd3a6be87850bdb7213f396ca5fea
twalder-docnet <tom@doc-net.com>
2015-06-18 11:24:29 +0100

    Revise test bootstrap for new Google SDK package

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,18 +8,8 @@
 // Time zone
 date_default_timezone_set('UTC');
 
-// Autoloader for GDS
+// Autoloader
 require_once(dirname(__FILE__) . '/../vendor/autoload.php');
-
-// Autoloader for the App Engine SDK
-foreach($_SERVER['argv'] as $str_arg) {
-    if(strpos($str_arg, '=')) {
-        $arr_kv_pair = explode('=', $str_arg, 2);
-        if('/google_appengine/php/sdk' == substr($arr_kv_pair[1], -25)) {
-            require($arr_kv_pair[1] . '/google/appengine/runtime/autoloader.php');
-        }
-    }
-}
 
 // Base Test Files
 require_once(dirname(__FILE__) . '/base/GDSTest.php');
