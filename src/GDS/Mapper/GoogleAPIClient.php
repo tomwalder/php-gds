97a5fda10c3d78125af0ce767a335ee4d5fcfda8
twalder-docnet <tom@doc-net.com>
2015-06-23 11:38:47 +0100

    Add string list to auto-detect types

--- src/GDS/Mapper/GoogleAPIClient.php
+++ src/GDS/Mapper/GoogleAPIClient.php
@@ -330,7 +330,7 @@ class GoogleAPIClient extends \GDS\Mapper
      */
     protected function extractAutoDetectValue($obj_property)
     {
-        foreach([Schema::PROPERTY_STRING, Schema::PROPERTY_INTEGER, Schema::PROPERTY_DATETIME, Schema::PROPERTY_DOUBLE, Schema::PROPERTY_BOOLEAN] as $int_field_type) {
+        foreach([Schema::PROPERTY_STRING, Schema::PROPERTY_INTEGER, Schema::PROPERTY_DATETIME, Schema::PROPERTY_DOUBLE, Schema::PROPERTY_BOOLEAN, Schema::PROPERTY_STRING_LIST] as $int_field_type) {
             $mix_val = $this->extractPropertyValue($int_field_type, $obj_property); // Recursive detection call
             if(NULL !== $mix_val) {
                 return $mix_val;
